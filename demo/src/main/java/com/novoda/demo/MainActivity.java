a174bf785dd06769ec1f426874b1ac86253cd9de
Hawazine H <hawazine@novoda.com>
2016-10-25 14:40:41 +0100

    Play a stream

--- demo/src/main/java/com/novoda/demo/MainActivity.java
+++ demo/src/main/java/com/novoda/demo/MainActivity.java
@@ -1,10 +1,13 @@
 package com.novoda.demo;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.novoda.noplayer.ContentType;
 import com.novoda.noplayer.NoPlayerView;
 import com.novoda.noplayer.Player;
+import com.novoda.noplayer.PlayerState;
 import com.novoda.noplayer.drm.DrmHandler;
 import com.novoda.noplayer.player.PlayerFactory;
 import com.novoda.noplayer.player.PrioritisedPlayers;
@@ -17,8 +20,18 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         NoPlayerView noPlayerView = (NoPlayerView) findViewById(R.id.player_view);
 
-        Player player = new PlayerFactory(this, PrioritisedPlayers.prioritiseExoPlayer()).create(DrmHandler.NO_DRM);
+        final Player player = new PlayerFactory(this, PrioritisedPlayers.prioritiseExoPlayer()).create(DrmHandler.NO_DRM);
+
         player.attach(noPlayerView);
+        player.addPreparedListener(new Player.PreparedListener() {
+            @Override
+            public void onPrepared(PlayerState playerMonitor) {
+                player.play();
+            }
+        });
+
+        Uri uri = Uri.parse("http://yt-dash-mse-test.commondatastorage.googleapis.com/media/car-20120827-85.mp4");
+        player.loadVideo(uri, ContentType.H264);
     }
 
 }
