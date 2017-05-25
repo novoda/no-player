ff5568ac1dcb2494d2a81b8054659e7c51c2f8d9
Ryan Feline <ryan.feline@novoda.com>
2017-05-25 16:33:47 +0100

    Remove calls to error and state changes from aspectRatioChangeForwarder.

--- core/src/main/java/com/novoda/noplayer/exoplayer/AspectRatioChangeForwarder.java
+++ core/src/main/java/com/novoda/noplayer/exoplayer/AspectRatioChangeForwarder.java
@@ -1,7 +1,5 @@
 package com.novoda.noplayer.exoplayer;
 
-import com.google.android.exoplayer2.ExoPlaybackException;
-
 public class AspectRatioChangeForwarder extends ExoPlayerEventForwarder {
 
     private final Listener listener;
@@ -10,18 +8,7 @@ public class AspectRatioChangeForwarder extends ExoPlayerEventForwarder {
         this.listener = listener;
     }
 
-    @Override
-    public void forwardPlayerStateChanged(boolean playWhenReady, int playbackState) {
-        //no-op
-
-    }
-
-    @Override
-    public void forwardPlayerError(ExoPlaybackException error) {
-        //no-op
-    }
-
-    @Override
+    @Overrides
     public void forwardVideoSizeChanged(int width, int height, int unappliedRotationDegrees, float pixelWidthHeightRatio) {
         float aspectRatio = determineAspectRatio(width, height, pixelWidthHeightRatio);
         listener.onNewAspectRatio(aspectRatio);
