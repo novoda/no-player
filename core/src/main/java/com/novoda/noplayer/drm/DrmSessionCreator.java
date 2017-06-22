b7314b1e5ec63897afa6d6b794b5e02ddc65d967
Ryan Feline <ryan.feline@novoda.com>
2017-06-22 08:32:35 +0100

    Mark null return method as Nullable.

--- core/src/main/java/com/novoda/noplayer/drm/DrmSessionCreator.java
+++ core/src/main/java/com/novoda/noplayer/drm/DrmSessionCreator.java
@@ -1,10 +1,13 @@
 package com.novoda.noplayer.drm;
 
+import android.support.annotation.Nullable;
+
 import com.google.android.exoplayer2.drm.DrmSessionManager;
 import com.google.android.exoplayer2.drm.FrameworkMediaCrypto;
 
 public interface DrmSessionCreator {
 
+    @Nullable
     DrmSessionManager<FrameworkMediaCrypto> create();
 
     class DrmSessionManagerCreationException extends RuntimeException {
