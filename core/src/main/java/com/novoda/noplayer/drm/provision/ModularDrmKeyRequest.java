92984d089960dbf52e578f1fbb514c3d85d2566a
Ryan Feline <ryan.feline@novoda.com>
2017-06-22 08:48:24 +0100

    Add toString, equals and hashcode.

--- core/src/main/java/com/novoda/noplayer/drm/provision/ModularDrmKeyRequest.java
+++ core/src/main/java/com/novoda/noplayer/drm/provision/ModularDrmKeyRequest.java
@@ -1,5 +1,7 @@
 package com.novoda.noplayer.drm.provision;
 
+import java.util.Arrays;
+
 public class ModularDrmKeyRequest {
 
     private final String url;
@@ -16,5 +18,37 @@ public class ModularDrmKeyRequest {
 
     public byte[] data() {
         return data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ModularDrmKeyRequest that = (ModularDrmKeyRequest) o;
+
+        if (url != null ? !url.equals(that.url) : that.url != null) {
+            return false;
+        }
+        return Arrays.equals(data, that.data);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = url != null ? url.hashCode() : 0;
+        result = 31 * result + Arrays.hashCode(data);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "ModularDrmKeyRequest{" +
+                "url='" + url + '\'' +
+                ", data=" + Arrays.toString(data) +
+                '}';
     }
 }
