371e7942961da9df9d4153a60aa0d792fbc8f29a
Ryan Feline <ryan.feline@novoda.com>
2017-08-14 11:26:27 +0100

    Add equals and hashcode to FrameworkDrmSession models.

--- core/src/main/java/com/novoda/noplayer/internal/exoplayer/drm/InvalidDrmSession.java
+++ core/src/main/java/com/novoda/noplayer/internal/exoplayer/drm/InvalidDrmSession.java
@@ -47,4 +47,23 @@ class InvalidDrmSession implements FrameworkDrmSession {
     public SessionId getSessionId() {
         return SessionId.absent();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        InvalidDrmSession that = (InvalidDrmSession) o;
+
+        return drmSessionException != null ? drmSessionException.equals(that.drmSessionException) : that.drmSessionException == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return drmSessionException != null ? drmSessionException.hashCode() : 0;
+    }
 }
--- core/src/main/java/com/novoda/noplayer/internal/exoplayer/drm/LocalDrmSession.java
+++ core/src/main/java/com/novoda/noplayer/internal/exoplayer/drm/LocalDrmSession.java
@@ -53,4 +53,32 @@ class LocalDrmSession implements FrameworkDrmSession {
     public SessionId getSessionId() {
         return sessionId;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        LocalDrmSession that = (LocalDrmSession) o;
+
+        if (mediaCrypto != null ? !mediaCrypto.equals(that.mediaCrypto) : that.mediaCrypto != null) {
+            return false;
+        }
+        if (keySetIdToRestore != null ? !keySetIdToRestore.equals(that.keySetIdToRestore) : that.keySetIdToRestore != null) {
+            return false;
+        }
+        return sessionId != null ? sessionId.equals(that.sessionId) : that.sessionId == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = mediaCrypto != null ? mediaCrypto.hashCode() : 0;
+        result = 31 * result + (keySetIdToRestore != null ? keySetIdToRestore.hashCode() : 0);
+        result = 31 * result + (sessionId != null ? sessionId.hashCode() : 0);
+        return result;
+    }
 }
