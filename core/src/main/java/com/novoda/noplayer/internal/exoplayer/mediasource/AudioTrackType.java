36127699ec04c9cbe57dc57ebf931b707e5c4571
Ryan Feline <ryan.feline@novoda.com>
2017-08-22 11:04:36 +0100

    Add AudioTrackType to perform mapping between selection flag and enum.

---
+++ core/src/main/java/com/novoda/noplayer/internal/exoplayer/mediasource/AudioTrackType.java
@@ -0,0 +1,22 @@
+package com.novoda.noplayer.internal.exoplayer.mediasource;
+
+public enum AudioTrackType {
+    MAIN(1),
+    AUDIO_DESCRIBED(0),
+    UNKNOWN(-1);
+
+    private final int selectionFlag;
+
+    AudioTrackType(int selectionFlag) {
+        this.selectionFlag = selectionFlag;
+    }
+
+    static AudioTrackType from(int selectionFlag) {
+        for (AudioTrackType audioTrackType : AudioTrackType.values()) {
+            if (audioTrackType.selectionFlag == selectionFlag) {
+                return audioTrackType;
+            }
+        }
+        return UNKNOWN;
+    }
+}
