2614d0ae589f3e708fc445897b65eea12b87415a
Ryan Feline <ryan.feline@novoda.com>
2017-08-24 09:54:41 +0100

    Switch selection flag values.

--- core/src/main/java/com/novoda/noplayer/internal/exoplayer/mediasource/AudioTrackType.java
+++ core/src/main/java/com/novoda/noplayer/internal/exoplayer/mediasource/AudioTrackType.java
@@ -2,8 +2,8 @@ package com.novoda.noplayer.internal.exoplayer.mediasource;
 
 public enum AudioTrackType {
 
-    MAIN(0),
-    ALTERNATIVE(1),
+    MAIN(1),
+    ALTERNATIVE(0),
     UNKNOWN(-1);
 
     private final int selectionFlag;
