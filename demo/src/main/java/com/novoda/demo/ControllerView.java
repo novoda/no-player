a0683f4594da721c7dfb4eb6f185bb9e426ce44b
Ryan Feline <ryan.feline@novoda.com>
2017-09-26 16:36:51 +0100

    Add dummy controls, without functionality.

--- demo/src/main/java/com/novoda/demo/ControllerView.java
+++ demo/src/main/java/com/novoda/demo/ControllerView.java
@@ -5,16 +5,24 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
-import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
-public class ControllerView extends FrameLayout {
+import com.novoda.notils.exception.DeveloperError;
 
-    public ControllerView(@NonNull Context context) {
-        super(context);
-    }
+public class ControllerView extends LinearLayout {
 
     public ControllerView(@NonNull Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        super(context, attrs, 0);
+    }
+
+    public ControllerView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        super.setOrientation(HORIZONTAL);
+    }
+
+    @Override
+    public final void setOrientation(int orientation) {
+        throw new DeveloperError("This layout only supports horizontal orientation");
     }
 
     @Override
