7ac0039fa476c85c3ba7859883fc0f5d9471d806
Ryan Feline <ryan.feline@novoda.com>
2017-11-13 20:03:15 +0000

    Use truth assertions over fest.

--- demo/src/test/java/com/novoda/demo/TimeFormatterTest.java
+++ demo/src/test/java/com/novoda/demo/TimeFormatterTest.java
@@ -2,7 +2,7 @@ package com.novoda.demo;
 
 import org.junit.Test;
 
-import static org.fest.assertions.api.Assertions.assertThat;
+import static com.google.common.truth.Truth.assertThat;
 
 public class TimeFormatterTest {
 
