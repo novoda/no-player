29405241bad2180a890372269fe5f0a9e48e9ff1
Ryan Feline <ryan.feline@novoda.com>
2018-04-23 09:38:37 +0100

    Add withMinDurationBeforeQualityIncreaseInMillis to the OptionsBuilder.

--- core/src/main/java/com/novoda/noplayer/Options.java
+++ core/src/main/java/com/novoda/noplayer/Options.java
@@ -3,12 +3,18 @@ package com.novoda.noplayer;
 public class Options {
 
     private final ContentType contentType;
+    private final long minDurationBeforeQualityIncreaseInMillis;
 
-    Options(ContentType contentType) {
+    Options(ContentType contentType, long minDurationBeforeQualityIncreaseInMillis) {
         this.contentType = contentType;
+        this.minDurationBeforeQualityIncreaseInMillis = minDurationBeforeQualityIncreaseInMillis;
     }
 
     public ContentType contentType() {
         return contentType;
+    }
+
+    public long minDurationBeforeQualityIncreaseInMillis() {
+        return minDurationBeforeQualityIncreaseInMillis;
     }
 }
--- core/src/main/java/com/novoda/noplayer/OptionsBuilder.java
+++ core/src/main/java/com/novoda/noplayer/OptionsBuilder.java
@@ -3,11 +3,14 @@ package com.novoda.noplayer;
 import android.net.Uri;
 
 /**
- * Builds instances of {@link Options} for {@link NoPlayer#loadVideo(Uri, ContentType)}.
+ * Builds instances of {@link Options} for {@link NoPlayer#loadVideo(Uri, Options)}.
  */
 public class OptionsBuilder {
 
+    private static final long DEFAULT_MIN_DURATION_FOR_QUALITY_INCREASE_MS = 10000;
+
     private ContentType contentType = ContentType.H264;
+    private long minDurationBeforeQualityIncreaseInMillis = DEFAULT_MIN_DURATION_FOR_QUALITY_INCREASE_MS;
 
     /**
      * Sets {@link OptionsBuilder} to build {@link Options} with a given {@link ContentType}.
@@ -21,8 +24,25 @@ public class OptionsBuilder {
         return this;
     }
 
+    /**
+     * Sets {@link OptionsBuilder} to build {@link Options} so that the {@link NoPlayer}
+     * switches to a higher quality video track after {@param minDurationInMillis}.
+     *
+     * @param minDurationInMillis before switching to a higher quality video track.
+     * @return {@link OptionsBuilder}.
+     */
+    public OptionsBuilder withMinDurationBeforeQualityIncreaseInMillis(long minDurationInMillis) {
+        this.minDurationBeforeQualityIncreaseInMillis = minDurationInMillis;
+        return this;
+    }
+
+    /**
+     * Builds a new {@link Options} instance.
+     *
+     * @return a {@link Options} instance.
+     */
     public Options build() {
-        return new Options(contentType);
+        return new Options(contentType, minDurationBeforeQualityIncreaseInMillis);
     }
 
 }
