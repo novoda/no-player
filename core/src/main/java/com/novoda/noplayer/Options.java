4c0f8cd942ba883b6b84340fae054fc18f5badd5
Ryan Feline <ryan.feline@novoda.com>
2018-06-14 12:05:01 +0100

    Add equals, hashcode and toString to Options.

--- core/src/main/java/com/novoda/noplayer/Options.java
+++ core/src/main/java/com/novoda/noplayer/Options.java
@@ -23,4 +23,41 @@ public class Options {
     public int maxInitialBitrate() {
         return maxInitialBitrate;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Options options = (Options) o;
+
+        if (minDurationBeforeQualityIncreaseInMillis != options.minDurationBeforeQualityIncreaseInMillis) {
+            return false;
+        }
+        if (maxInitialBitrate != options.maxInitialBitrate) {
+            return false;
+        }
+        return contentType == options.contentType;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = contentType != null ? contentType.hashCode() : 0;
+        result = 31 * result + minDurationBeforeQualityIncreaseInMillis;
+        result = 31 * result + maxInitialBitrate;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Options{"
+                + "contentType=" + contentType
+                + ", minDurationBeforeQualityIncreaseInMillis=" + minDurationBeforeQualityIncreaseInMillis
+                + ", maxInitialBitrate=" + maxInitialBitrate
+                + '}';
+    }
 }
