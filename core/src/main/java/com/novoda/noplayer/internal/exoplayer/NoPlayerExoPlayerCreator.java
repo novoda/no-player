19aad9fc49ca7fc9a5568dc87b2a4fa1593eb46f
Ryan Feline <ryan.feline@novoda.com>
2018-12-12 18:00:13 +0000

    Use media selector with fallback.

--- core/src/main/java/com/novoda/noplayer/internal/exoplayer/NoPlayerExoPlayerCreator.java
+++ core/src/main/java/com/novoda/noplayer/internal/exoplayer/NoPlayerExoPlayerCreator.java
@@ -55,7 +55,7 @@ public class NoPlayerExoPlayerCreator {
 
             MediaCodecSelector mediaCodecSelector = downgradeSecureDecoder
                     ? SecurityDowngradingCodecSelector.newInstance()
-                    : MediaCodecSelector.DEFAULT;
+                    : MediaCodecSelector.DEFAULT_WITH_FALLBACK;
 
             CompositeTrackSelectorCreator trackSelectorCreator = new CompositeTrackSelectorCreator();
 
