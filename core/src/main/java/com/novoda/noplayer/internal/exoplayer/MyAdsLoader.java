b5ca3a30b8a411dc8797e62b0f8080d3aa307a47
Ryan Feline <ryan.feline@novoda.com>
2019-03-14 15:10:25 +0000

    Remove unnecessary logs and interface methods that have defaults.

--- core/src/main/java/com/novoda/noplayer/internal/exoplayer/MyAdsLoader.java
+++ core/src/main/java/com/novoda/noplayer/internal/exoplayer/MyAdsLoader.java
@@ -3,16 +3,11 @@ package com.novoda.noplayer.internal.exoplayer;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.annotation.Nullable;
-import android.util.Log;
 
-import com.google.android.exoplayer2.ExoPlaybackException;
-import com.google.android.exoplayer2.PlaybackParameters;
 import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.Timeline;
-import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.source.ads.AdPlaybackState;
 import com.google.android.exoplayer2.source.ads.AdsLoader;
-import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.novoda.noplayer.AdvertBreak;
 import com.novoda.noplayer.AdvertsLoader;
 
@@ -39,17 +34,13 @@ public class MyAdsLoader implements AdsLoader, Player.EventListener {
 
     @Override
     public void setSupportedContentTypes(int... contentTypes) {
-        for (int contentType : contentTypes) {
-            Log.e("LOADER", "setSupportedContentTypes: " + contentType);
-        }
+        // no-op
     }
 
     @Override
     public void start(final EventListener eventListener, AdViewProvider adViewProvider) {
-        Log.e("LOADER", "Starting load");
         this.eventListener = eventListener;
         if (adPlaybackState == null) {
-            Log.e("LOADER", "calling client load");
             loader.load(advertsLoadedCallback);
         }
     }
@@ -59,12 +50,9 @@ public class MyAdsLoader implements AdsLoader, Player.EventListener {
         public void onAdvertsLoaded(List<AdvertBreak> advertBreaks) {
             adPlaybackState = AdvertPlaybackState.from(advertBreaks);
 
-            Log.e("LOADER", "retrieved adverts");
-
             new Handler(Looper.getMainLooper()).post(new Runnable() {
                 @Override
                 public void run() {
-                    Log.e("LOADER", "send to main thread");
                     updateAdPlaybackState();
                 }
             });
@@ -73,20 +61,17 @@ public class MyAdsLoader implements AdsLoader, Player.EventListener {
         @Override
         public void onAdvertsError(String message) {
             eventListener.onAdLoadError(null, null);
-            Log.e("LOADER", "fail: " + message);
         }
     };
 
     private void updateAdPlaybackState() {
         if (eventListener != null) {
-            Log.e("LOADER", "playback state: " + adPlaybackState);
             eventListener.onAdPlaybackState(adPlaybackState);
         }
     }
 
     @Override
     public void stop() {
-        Log.e("LOADER", "Stopping load");
         if (adPlaybackState != null && player != null && adIndexInGroup != -1 && adGroupIndex != -1) {
             adPlaybackState = adPlaybackState.withAdResumePositionUs(TimeUnit.MILLISECONDS.toMicros(player.getCurrentPosition()));
         }
@@ -95,7 +80,6 @@ public class MyAdsLoader implements AdsLoader, Player.EventListener {
 
     @Override
     public void setPlayer(@Nullable Player player) {
-        Log.e("LOADER", "setPlayer");
         if (player != null) {
             this.player = player;
             this.player.addListener(this);
@@ -104,7 +88,10 @@ public class MyAdsLoader implements AdsLoader, Player.EventListener {
 
     @Override
     public void release() {
-        Log.e("LOADER", "release");
+        if (player != null) {
+            player.removeListener(this);
+        }
+
         adPlaybackState = null;
         player = null;
     }
@@ -112,72 +99,25 @@ public class MyAdsLoader implements AdsLoader, Player.EventListener {
     @Override
     public void handlePrepareError(int adGroupIndex, int adIndexInAdGroup, IOException exception) {
         if (adPlaybackState != null) {
-            Log.e("LOADER", "group: " + adGroupIndex + " ad: " + adIndexInAdGroup + " handlePrepareError: " + exception);
             adPlaybackState = adPlaybackState.withAdLoadError(adGroupIndex, adIndexInAdGroup);
         }
     }
 
     @Override
     public void onTimelineChanged(Timeline timeline, @Nullable Object manifest, int reason) {
-        Log.e("LOADER", "Timeline changed");
         if (reason == Player.TIMELINE_CHANGE_REASON_RESET) {
             // The player is being reset and this source will be released.
             return;
         }
 
         if (player != null) {
-            Log.e("LOADER", "reason " + reason + " contentPosition " + player.getContentPosition() + " currentPosition " + player.getCurrentPosition() + " adIndex " + player.getCurrentAdIndexInAdGroup() + " adGroup " + player.getCurrentAdGroupIndex());
             adGroupIndex = player.getCurrentAdGroupIndex();
             adIndexInGroup = player.getCurrentAdIndexInAdGroup();
         }
     }
 
     @Override
-    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
-        Log.e("LOADER", "Track changed");
-    }
-
-    @Override
-    public void onLoadingChanged(boolean isLoading) {
-        Log.e("LOADER", "Loading changed");
-    }
-
-    @Override
-    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-        Log.e("LOADER", "PlayerState changed");
-    }
-
-    @Override
-    public void onRepeatModeChanged(int repeatMode) {
-        Log.e("LOADER", "RepeatMode changed");
-    }
-
-    @Override
-    public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
-        Log.e("LOADER", "ShuffleMode changed");
-    }
-
-    @Override
-    public void onPlayerError(ExoPlaybackException error) {
-        Log.e("LOADER", "PlayerError " + error.getMessage());
-    }
-
-    @Override
-    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
-        Log.e("LOADER", "Playback params changed");
-    }
-
-    @Override
-    public void onSeekProcessed() {
-        Log.e("LOADER", "Seek processed");
-    }
-
-    @Override
     public void onPositionDiscontinuity(int reason) {
-        Log.e("LOADER", "Position Discontinuity ");
-        if (player != null) {
-            Log.e("LOADER", " reason " + reason + " contentPosition " + player.getContentPosition() + " currentPosition " + player.getCurrentPosition() + " adIndex " + player.getCurrentAdIndexInAdGroup() + " adGroup " + player.getCurrentAdGroupIndex());
-        }
         if (reason == Player.DISCONTINUITY_REASON_AD_INSERTION && player != null && adPlaybackState != null) {
             if (adGroupIndex != -1 && adIndexInGroup != -1) {
                 adPlaybackState = adPlaybackState.withPlayedAd(adGroupIndex, adIndexInGroup);
