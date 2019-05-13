ea43ed6e3b7cb82a052adb82c49585ef9a79b06c
Ferran Garriga <ferran@novoda.com>
2019-05-13 15:20:41 +0100

    enable software decodec fallback

--- core/src/main/java/com/novoda/noplayer/internal/exoplayer/SimpleRenderersFactory.java
+++ core/src/main/java/com/novoda/noplayer/internal/exoplayer/SimpleRenderersFactory.java
@@ -55,6 +55,7 @@ class SimpleRenderersFactory implements RenderersFactory {
     private static final boolean DO_NOT_PLAY_CLEAR_SAMPLES_WITHOUT_KEYS = false;
     private static final boolean INIT_ARGS = true;
     private static final boolean PLAY_CLEAR_SAMPLES_WITHOUT_KEYS = true;
+    public static final boolean ENABLE_DECODER_FALLBACK = true;
 
     /**
      * Modes for using extension renderers.
@@ -165,6 +166,7 @@ class SimpleRenderersFactory implements RenderersFactory {
                 allowedVideoJoiningTimeMs,
                 drmSessionManager,
                 DO_NOT_PLAY_CLEAR_SAMPLES_WITHOUT_KEYS,
+                ENABLE_DECODER_FALLBACK,
                 eventHandler,
                 eventListener,
                 MAX_DROPPED_VIDEO_FRAME_COUNT_TO_NOTIFY));
