8381dc4d6d6bd13a734182053f589a13642c4373
Ryan Feline <ryan.feline@novoda.com>
2019-05-16 16:47:41 +0100

    Clear the track listeners.

--- core/src/main/java/com/novoda/noplayer/internal/listeners/PlayerListenersHolder.java
+++ core/src/main/java/com/novoda/noplayer/internal/listeners/PlayerListenersHolder.java
@@ -220,5 +220,6 @@ public class PlayerListenersHolder implements Listeners {
         heartbeatCallbacks.clear();
         droppedFramesListeners.clear();
         advertListeners.clear();
+        tracksChangedListeners.clear();
     }
 }
