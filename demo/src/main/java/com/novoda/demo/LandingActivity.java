116c22847c11645ddd519ebd297c0508574c29b7
Ryan Feline <ryan.feline@novoda.com>
2019-06-19 15:32:29 +0100

    Add checkbox to represent downloaded DRM.

--- demo/src/main/java/com/novoda/demo/LandingActivity.java
+++ demo/src/main/java/com/novoda/demo/LandingActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,17 +14,16 @@ public class LandingActivity extends AppCompatActivity {
 
     public static final String KEY_MPD_ADDRESS = "MPD_ADDRESS";
     public static final String KEY_LICENSE_SERVER_ADDRESS = "LICENSE_SERVER_ADDRESS";
-
-    private EditText mpdAddress;
-    private EditText licenseAddress;
+    public static final String KEY_DOWNLOAD_LICENSE = "DOWNLOAD_LICENSE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
 
-        mpdAddress = findViewById(R.id.mpd_address);
-        licenseAddress = findViewById(R.id.license_address);
+        final EditText mpdAddress = findViewById(R.id.mpd_address);
+        final EditText licenseAddress = findViewById(R.id.license_address);
+        final CheckBox downloadLicense = findViewById(R.id.download_license);
 
         Button playUsingProperties = findViewById(R.id.button_custom_uri);
 
@@ -35,6 +35,7 @@ public class LandingActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class)
                         .putExtra(KEY_MPD_ADDRESS, mpdAddress.getText().toString())
                         .putExtra(KEY_LICENSE_SERVER_ADDRESS, licenseAddress.getText().toString())
+                        .putExtra(KEY_DOWNLOAD_LICENSE, downloadLicense.isChecked())
                         .setData(Uri.parse(customUri));
 
                 startActivity(intent);
