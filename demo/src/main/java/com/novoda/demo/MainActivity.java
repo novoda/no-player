31e89e6e84e0ecda6da70bef689c47e4007557e7
Ryan Feline <ryan.feline@novoda.com>
2019-06-19 15:24:56 +0100

    Add defaults to the landing activity.

--- demo/src/main/java/com/novoda/demo/MainActivity.java
+++ demo/src/main/java/com/novoda/demo/MainActivity.java
@@ -24,8 +24,6 @@ import java.util.List;
 
 public class MainActivity extends Activity {
 
-    private static final String URI_VIDEO_WIDEVINE_EXAMPLE_MODULAR_MPD = "https://storage.googleapis.com/wvmedia/cenc/hevc/tears/tears.mpd";
-    private static final String EXAMPLE_MODULAR_LICENSE_SERVER_PROXY = "https://proxy.uat.widevine.com/proxy?provider=widevine_test";
     private static final int HALF_A_SECOND_IN_MILLIS = 500;
     private static final int TWO_MEGABITS = 2000000;
     private static final int MAX_VIDEO_BITRATE = 800000;
@@ -34,7 +32,9 @@ public class MainActivity extends Activity {
     private DemoPresenter demoPresenter;
     private DialogCreator dialogCreator;
     private CheckBox hdSelectionCheckBox;
-    private Uri mpdAddress = Uri.parse(URI_VIDEO_WIDEVINE_EXAMPLE_MODULAR_MPD);
+
+    private Uri mpdAddress;
+    private String licenseServerAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,7 +56,7 @@ public class MainActivity extends Activity {
         subtitleSelectionButton.setOnClickListener(showSubtitleSelectionDialog);
         hdSelectionCheckBox.setOnCheckedChangeListener(toggleHdSelection);
 
-        DataPostingModularDrm drmHandler = new DataPostingModularDrm(EXAMPLE_MODULAR_LICENSE_SERVER_PROXY);
+        DataPostingModularDrm drmHandler = new DataPostingModularDrm(licenseServerAddress);
 
         player = new PlayerBuilder()
                 .withWidevineModularStreamingDrm(drmHandler)
@@ -99,16 +99,13 @@ public class MainActivity extends Activity {
                 }
             }
         });
-
     }
 
     private void extractFromIntent() {
         Intent intent = getIntent();
         if (intent != null) {
-            String mpdAddress = intent.getStringExtra(LandingActivity.KEY_MPD_ADDRESS);
-            if (mpdAddress != null && !mpdAddress.isEmpty()) {
-                this.mpdAddress = Uri.parse(mpdAddress);
-            }
+            mpdAddress = Uri.parse(intent.getStringExtra(LandingActivity.KEY_MPD_ADDRESS));
+            licenseServerAddress = intent.getStringExtra(LandingActivity.KEY_LICENSE_SERVER_ADDRESS);
         }
     }
 
