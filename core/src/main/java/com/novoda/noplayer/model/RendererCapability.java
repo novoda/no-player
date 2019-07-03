65efbde35dd6c56e72bd751c5d7e83a054f3655e
Ferran Garriga <ferran@novoda.com>
2019-07-03 11:26:20 +0100

    add missing renderer capability type

--- core/src/main/java/com/novoda/noplayer/model/RendererCapability.java
+++ core/src/main/java/com/novoda/noplayer/model/RendererCapability.java
@@ -3,6 +3,7 @@ package com.novoda.noplayer.model;
 import com.google.android.exoplayer2.RendererCapabilities;
 
 public enum RendererCapability {
+    FORMAT_HANDLED(RendererCapabilities.FORMAT_HANDLED),
     FORMAT_EXCEEDS_CAPABILITIES(RendererCapabilities.FORMAT_EXCEEDS_CAPABILITIES),
     FORMAT_UNSUPPORTED_DRM(RendererCapabilities.FORMAT_UNSUPPORTED_DRM),
     FORMAT_UNSUPPORTED_SUBTYPE(RendererCapabilities.FORMAT_UNSUPPORTED_SUBTYPE),
