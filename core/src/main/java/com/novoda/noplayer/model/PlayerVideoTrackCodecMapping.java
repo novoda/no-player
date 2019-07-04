ce4aeeb3c7ed84152f469b848be7716d2d38e792
Ferran Garriga <ferran@novoda.com>
2019-07-04 17:31:24 +0100

    add javadoc

--- core/src/main/java/com/novoda/noplayer/model/PlayerVideoTrackCodecMapping.java
+++ core/src/main/java/com/novoda/noplayer/model/PlayerVideoTrackCodecMapping.java
@@ -9,9 +9,13 @@ import java.util.Map;
  * This class holds an association betwen the codec being used by a particular track
  * and the decoder that the engine assigns to this codec.
  *
- *
+ * Bear in mind that this association is provisional and there is no guarantee that
+ * the engine will fallback to another decoder. This is due to that usually several
+ * decoders are provided for a particual track. If the first decoder fails to initialise
+ * then the engine will choose another decoder. This information is not easely accessible
+ * and so we only provide the first association that the engine does.
  */
-public class PlayerVideoTrackCodecMapping {
+public final class PlayerVideoTrackCodecMapping {
 
     private static final PlayerVideoTrackCodecMapping INSTANCE = LazySingleton.INSTANCE;
 
