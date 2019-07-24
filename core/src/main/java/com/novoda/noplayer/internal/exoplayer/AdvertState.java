ff5875c9e7289364d8b25462a535dda45f60478b
Ryan Feline <ryan.feline@novoda.com>
2019-07-24 11:36:21 +0100

    Port over work already completed on AdvertStateTest.

---
+++ core/src/main/java/com/novoda/noplayer/internal/exoplayer/AdvertState.java
@@ -0,0 +1,100 @@
+package com.novoda.noplayer.internal.exoplayer;
+
+import com.novoda.noplayer.Advert;
+import com.novoda.noplayer.AdvertBreak;
+import com.novoda.noplayer.NoPlayer;
+
+import java.util.List;
+
+class AdvertState {
+
+    interface Callback {
+
+        void onAdvertPlayed(int advertBreakIndex, int advertIndex);
+
+        void onAdvertsDisabled();
+    }
+
+    private static final int INVALID_INDEX = -1;
+
+    private final List<AdvertBreak> advertBreaks;
+    private final NoPlayer.AdvertListener advertListener;
+    private final Callback callback;
+
+    private int advertIndex = -1;
+    private int advertBreakIndex = -1;
+    private boolean playingAdvert;
+    private boolean advertsDisabled;
+
+    AdvertState(List<AdvertBreak> advertBreaks, NoPlayer.AdvertListener advertListener, Callback callback) {
+        this.advertBreaks = advertBreaks;
+        this.advertListener = advertListener;
+        this.callback = callback;
+    }
+
+    void update(boolean isPlayingAdvert, int currentAdvertBreakIndex, int currentAdvertIndex) {
+        boolean wasPlayingAd = playingAdvert;
+        playingAdvert = isPlayingAdvert;
+
+        int previousAdvertBreakIndex = advertBreakIndex;
+        int previousAdvertIndex = advertIndex;
+
+        advertBreakIndex = playingAdvert ? currentAdvertBreakIndex : INVALID_INDEX;
+        advertIndex = playingAdvert ? currentAdvertIndex : INVALID_INDEX;
+        boolean advertFinished = wasPlayingAd && advertIndex != previousAdvertIndex;
+
+        if (advertFinished) {
+            callback.onAdvertPlayed(previousAdvertBreakIndex, previousAdvertIndex);
+            notifyAdvertEnd(previousAdvertBreakIndex, previousAdvertIndex);
+        }
+
+        boolean advertStarted = isPlayingAdvert && advertIndex != previousAdvertIndex;
+        if (advertStarted) {
+            notifyAdvertStart(advertBreakIndex, advertIndex);
+        }
+    }
+
+    private void notifyAdvertEnd(int playedAdvertBreakIndex, int playedAdvertIndex) {
+        AdvertBreak advertBreak = advertBreaks.get(playedAdvertBreakIndex);
+        List<Advert> adverts = advertBreak.adverts();
+        advertListener.onAdvertEnd(adverts.get(playedAdvertIndex));
+
+        if (advertBreakIndex != playedAdvertBreakIndex) {
+            advertListener.onAdvertBreakEnd(advertBreak);
+            resetState();
+        }
+    }
+
+    private void resetState() {
+        advertBreakIndex = -1;
+        advertIndex = -1;
+        playingAdvert = false;
+    }
+
+    private void notifyAdvertStart(int advertBreakIndex, int advertIndex) {
+        AdvertBreak advertBreak = advertBreaks.get(advertBreakIndex);
+
+        if (advertIndex == 0) {
+            advertListener.onAdvertBreakStart(advertBreak);
+        }
+
+        Advert advert = advertBreak.adverts().get(advertIndex);
+        advertListener.onAdvertStart(advert);
+    }
+
+    void disableAdverts() {
+        if (advertsDisabled) {
+            return;
+        }
+
+        resetState();
+        advertsDisabled = true;
+        callback.onAdvertsDisabled();
+        advertListener.onAdvertsDisabled();
+    }
+
+    void enableAdverts() {
+
+    }
+
+}
---
+++ core/src/test/java/com/novoda/noplayer/internal/exoplayer/AdvertStateTest.java
@@ -0,0 +1,139 @@
+package com.novoda.noplayer.internal.exoplayer;
+
+import com.novoda.noplayer.Advert;
+import com.novoda.noplayer.AdvertBreak;
+import com.novoda.noplayer.NoPlayer;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
+
+import static com.novoda.noplayer.AdvertBreakFixtures.anAdvertBreak;
+import static com.novoda.noplayer.AdvertFixtures.anAdvert;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+
+public class AdvertStateTest {
+
+    private static final boolean IS_NOT_PLAYING_ADVERT = false;
+    private static final boolean IS_PLAYING_ADVERT = true;
+    private static final int UNSET_ADVERT_BREAK_INDEX = -1;
+    private static final int UNSET_ADVERT_INDEX = -1;
+    private static final int ONE_SECOND_IN_MILLIS = 1000;
+    private static final int TWO_SECONDS_IN_MILLIS = 2000;
+    private static final int THREE_SECONDS_IN_MILLIS = 3000;
+
+    private static final Advert FIRST_ADVERT = anAdvert()
+            .withDurationInMillis(ONE_SECOND_IN_MILLIS)
+            .build();
+    private static final Advert SECOND_ADVERT = anAdvert()
+            .withDurationInMillis(TWO_SECONDS_IN_MILLIS)
+            .build();
+    private static final Advert THIRD_ADVERT = anAdvert()
+            .withDurationInMillis(THREE_SECONDS_IN_MILLIS)
+            .build();
+
+    private static final AdvertBreak FIRST_ADVERT_BREAK = anAdvertBreak()
+            .withStartTimeInMillis(ONE_SECOND_IN_MILLIS)
+            .withAdvert(FIRST_ADVERT)
+            .build();
+    private static final AdvertBreak SECOND_ADVERT_BREAK = anAdvertBreak()
+            .withStartTimeInMillis(TWO_SECONDS_IN_MILLIS)
+            .withAdverts(FIRST_ADVERT, SECOND_ADVERT)
+            .build();
+    private static final AdvertBreak THIRD_ADVERT_BREAK = anAdvertBreak()
+            .withStartTimeInMillis(THREE_SECONDS_IN_MILLIS)
+            .withAdverts(FIRST_ADVERT, SECOND_ADVERT, THIRD_ADVERT)
+            .build();
+
+    private final List<AdvertBreak> ADVERT_BREAKS = Arrays.asList(FIRST_ADVERT_BREAK, SECOND_ADVERT_BREAK, THIRD_ADVERT_BREAK);
+    private final NoPlayer.AdvertListener advertListener = mock(NoPlayer.AdvertListener.class);
+    private final AdvertState.Callback callback = mock(AdvertState.Callback.class);
+
+    private final AdvertState advertState = new AdvertState(
+            ADVERT_BREAKS,
+            advertListener,
+            callback
+    );
+
+    @Test
+    public void emitsNoEvents_whenNotPlayingAdvert() {
+        advertState.update(IS_NOT_PLAYING_ADVERT, UNSET_ADVERT_BREAK_INDEX, UNSET_ADVERT_INDEX);
+
+        then(advertListener).shouldHaveZeroInteractions();
+        then(callback).shouldHaveZeroInteractions();
+    }
+
+    @Test
+    public void notifiesStartOfAdvertBreak_andAdvert_whenPlayingAdvert() {
+        advertState.update(IS_PLAYING_ADVERT, 0, 0);
+
+        InOrder inOrder = Mockito.inOrder(advertListener);
+        then(advertListener).should(inOrder).onAdvertBreakStart(FIRST_ADVERT_BREAK);
+        then(advertListener).should(inOrder).onAdvertStart(FIRST_ADVERT);
+        then(advertListener).shouldHaveNoMoreInteractions();
+        then(callback).shouldHaveZeroInteractions();
+    }
+
+    @Test
+    public void emitsNoAdditionalEvents_whenCallingWithSameAdvertIndex() {
+        advertState.update(IS_PLAYING_ADVERT, 0, 0);
+        Mockito.reset(advertListener, callback);
+        advertState.update(IS_PLAYING_ADVERT, 0, 0);
+
+        then(advertListener).shouldHaveZeroInteractions();
+        then(callback).shouldHaveZeroInteractions();
+    }
+
+    @Test
+    public void notifiesEndAdvertBreak_andAdvert_whenPlayingContent() {
+        advertState.update(IS_PLAYING_ADVERT, 0, 0);
+        Mockito.reset(advertListener, callback);
+        advertState.update(IS_NOT_PLAYING_ADVERT, UNSET_ADVERT_BREAK_INDEX, UNSET_ADVERT_INDEX);
+
+        InOrder inOrder = Mockito.inOrder(advertListener, callback);
+        then(callback).should(inOrder).onAdvertPlayed(0, 0);
+        then(advertListener).should(inOrder).onAdvertEnd(FIRST_ADVERT);
+        then(advertListener).should(inOrder).onAdvertBreakEnd(FIRST_ADVERT_BREAK);
+        then(callback).shouldHaveNoMoreInteractions();
+        then(advertListener).shouldHaveNoMoreInteractions();
+    }
+
+    @Test
+    public void transitionsBetweenAdverts() {
+        advertState.update(IS_PLAYING_ADVERT, 1, 0);
+        Mockito.reset(advertListener, callback);
+        advertState.update(IS_PLAYING_ADVERT, 1, 1);
+
+        InOrder inOrder = Mockito.inOrder(advertListener, callback);
+        then(callback).should(inOrder).onAdvertPlayed(1, 0);
+        then(advertListener).should(inOrder).onAdvertEnd(FIRST_ADVERT);
+        then(advertListener).should(inOrder).onAdvertStart(SECOND_ADVERT);
+        then(callback).shouldHaveNoMoreInteractions();
+        then(advertListener).shouldHaveNoMoreInteractions();
+    }
+
+    @Test
+    public void notifiesAdvertsDisabled() {
+        advertState.disableAdverts();
+
+        InOrder inOrder = Mockito.inOrder(advertListener, callback);
+        then(callback).should(inOrder).onAdvertsDisabled();
+        then(advertListener).should(inOrder).onAdvertsDisabled();
+        then(callback).shouldHaveNoMoreInteractions();
+        then(advertListener).shouldHaveNoMoreInteractions();
+    }
+
+    @Test
+    public void emitsNoEvents_whenAlreadyDisabled() {
+        advertState.disableAdverts();
+        Mockito.reset(advertListener, callback);
+        advertState.disableAdverts();
+
+        then(advertListener).shouldHaveZeroInteractions();
+        then(callback).shouldHaveZeroInteractions();
+    }
+}
