0334e629a8a93d9843336fcae4e5c6d8aa2ba723
Ferran Garriga <zegnus@gmail.com>
2020-01-21 17:16:52 +0000

    allow dash and hls streams

--- demo-new-exoplayer/src/main/java/com/novoda/demonewexoplayer/MainActivity.kt
+++ demo-new-exoplayer/src/main/java/com/novoda/demonewexoplayer/MainActivity.kt
@@ -3,10 +3,16 @@ package com.novoda.demonewexoplayer
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.drm.DefaultDrmSessionManager
+import com.google.android.exoplayer2.drm.DrmSessionManager
+import com.google.android.exoplayer2.drm.ExoMediaCrypto
 import com.google.android.exoplayer2.drm.HttpMediaDrmCallback
+import com.google.android.exoplayer2.source.BaseMediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.source.dash.DashMediaSource
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
 import com.google.android.exoplayer2.util.Util
@@ -35,7 +41,7 @@ class MainActivity : AppCompatActivity() {
         exoPlayer.prepare(mediaSource)
     }
 
-    private fun mediaSource(licenseUrl: String, manifestUrl: String): DashMediaSource {
+    private fun mediaSource(licenseUrl: String, manifestUrl: String): BaseMediaSource {
         val defaultHttpDataSourceFactory = DefaultHttpDataSourceFactory(Util.getUserAgent(this, "demo new exoplayer"))
         val mediaDrmCallback = HttpMediaDrmCallback(
                 licenseUrl,
@@ -50,9 +56,9 @@ class MainActivity : AppCompatActivity() {
                 defaultHttpDataSourceFactory
         )
         val uriDashManifest: Uri = Uri.parse(manifestUrl)
-        return DashMediaSource.Factory(factory)
-                .setDrmSessionManager(drmSessionManager)
-                .createMediaSource(uriDashManifest)
+
+        return Util.inferContentType(uriDashManifest)
+                .toMediaSource(factory, drmSessionManager, uriDashManifest)
     }
 
     override fun onDestroy() {
@@ -60,3 +66,15 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
     }
 }
+
+private fun Int.toMediaSource(
+        factory: DefaultDataSourceFactory,
+        drmSessionManager: DrmSessionManager<ExoMediaCrypto>,
+        uriDashManifest: Uri
+): BaseMediaSource =
+        when (this) {
+            C.TYPE_DASH -> DashMediaSource.Factory(factory).setDrmSessionManager(drmSessionManager).createMediaSource(uriDashManifest)
+            C.TYPE_HLS -> HlsMediaSource.Factory(factory).setDrmSessionManager(drmSessionManager).createMediaSource(uriDashManifest)
+            C.TYPE_OTHER -> ProgressiveMediaSource.Factory(factory).setDrmSessionManager(drmSessionManager).createMediaSource(uriDashManifest)
+            else -> throw IllegalStateException("Unsupported type $this")
+        }
